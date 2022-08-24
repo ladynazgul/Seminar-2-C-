b490c0aa6e7974df10b1790e0aa4c7731e4bf266
ladynazgool <ladynazgool@gmail.com>
2022-08-24 15:32:38 +0200

    Task-01 added

---
+++ Task-01/Program.cs
@@ -0,0 +1,8 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23
+
+int number = Convert.ToInt32(Console.ReadLine() ?? "0");
+if(number%7 == 0 && number%23 == 0)
+    Console.WriteLine("Число кратно и 7 и 23");
+else
+    Console.WriteLine("Число не кратно 7 или 23");
+    
