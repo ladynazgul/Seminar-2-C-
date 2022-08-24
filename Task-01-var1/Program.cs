97976a7c6d2693301eed32b7e0a900f4a253fbc3
ladynazgool <ladynazgool@gmail.com>
2022-08-24 16:07:12 +0200

    Task variant added

---
+++ Task-01-var1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Это вариант решения задачи на проверку кратности с использованием метода
+
+bool IsDivisible( int x, int n )
+{
+   return (x % n) == 0;
+}
+ 
+Console.WriteLine("Введите заданное число: ");
+int x = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите число-делитель: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+bool result = IsDivisible( x, n );
+
+Console.WriteLine(result);
