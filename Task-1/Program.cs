1923c42cc8cd4f5be71e0741fad74f34774f471c
ladynazgool <ladynazgool@gmail.com>
2022-08-25 03:21:25 +0200

    Task-1 added

---
+++ Task-1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+Console.WriteLine("Введите номер дня недели: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+if ( number > 5 )
+    Console.WriteLine("Это выходной день");
+
+else
+    Console.WriteLine("Будний день");
