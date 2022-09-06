c641ad9268152c576d48b23f20d9e333adf5a366
ladynazgool <ladynazgool@gmail.com>
2022-09-06 17:22:55 +0400

    Task-1 changed

--- Task-1/Program.cs
+++ Task-1/Program.cs
@@ -3,11 +3,11 @@
 Console.WriteLine("Введите номер дня недели: ");
 int number = int.Parse(Console.ReadLine() ?? "0");
 
-if ( ( number == 6 ) || ( number == 7 ) )
-    Console.WriteLine("Это выходной день");
-
 if (number <= 5)
     Console.WriteLine("Будний день");
+
+else if ( ( number == 6 ) || ( number == 7 ) )
+    Console.WriteLine("Это выходной день");
 
 else
     Console.WriteLine("В неделе только 7 дней, идиот!");
